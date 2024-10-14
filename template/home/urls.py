f21ed0073fbe42bb20ccb2d1210f36f76d6be474
lmnitzsche <logannitzsche1@gmail.com>
2024-10-14 16:13:11 -0500

    Privacy Policy Debugging

--- template/home/urls.py
+++ template/home/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    # URL for the privacy policy
+    path('privacy-policy/', TemplateView.as_view(template_name='home/privacyPolicy.html'), name='privacy_policy'),
 ]
