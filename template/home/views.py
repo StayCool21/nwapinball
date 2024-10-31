0740d59a4ab01ac81b059a16a2b625db807f5f24
Luke Welsh <21pinball21@protonmail.com>
2024-10-30 21:10:15 -0500

    Update address

--- template/home/views.py
+++ template/home/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth.views import LoginView, PasswordChangeView, PasswordResetView, PasswordResetConfirmView
 from theme_pixel.forms import RegistrationForm, UserLoginForm, UserPasswordResetForm, UserPasswordChangeForm, UserSetPasswordForm
 from django.contrib.auth import logout
+from google.cloud import firestore
 
 # Health check
 def health_check(request):
